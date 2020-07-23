3b1dbb19cf86793f1863139eed16e46446880b5b
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-07-23 23:43:41 +0530

    Adding some extra styling to the project

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import logoUrl from "../../asset/earth.svg";
+
 import "./header.styles.scss";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="u-container">
-        <h1 className="main-heading"> Where in the world?</h1>
+        <div className="logo-box">
+          <img src={logoUrl} alt="Logo" />
+        </div>
+        <h1 className="main-heading"> Atlas </h1>
       </div>
     </div>
   );
